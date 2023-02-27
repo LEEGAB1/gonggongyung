bc10de7807cca3020bba61652aeadf7cc9338aec
sumin <sumin>
2023-02-27 09:00:12 +0900

    비용 순위 수정

--- gogo/src/GasStation/GasStationServlet.java
+++ gogo/src/GasStation/GasStationServlet.java
@@ -23,84 +23,86 @@ import Service.map.GasStationServiceImpl;
 
 @WebServlet({ "/gogo/gasstation", "/gogo/gasstation/*" })
 public class GasStationServlet extends HttpServlet {
-	private GasStationService gasService;
+   private GasStationService gasService;
 
-	@Override
-	public void init(ServletConfig config) throws ServletException {
-		gasService = new GasStationServiceImpl(new GasStationDAOImpl());
+   @Override
+   public void init(ServletConfig config) throws ServletException {
+      gasService = new GasStationServiceImpl(new GasStationDAOImpl());
 
-	}
+   }
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String storename = req.getParameter("name");
-		System.out.println("name" + storename);
-		
-		if(storename == null) {
-			List<String> list = gasService.readGasXY();
-			// x,y 표값 받아온다
-			// 맵핑해서 보낸다
-			
-			ObjectMapper mapper = new ObjectMapper();
-			String json = mapper.writeValueAsString(list);
-			
-			PrintWriter pw = resp.getWriter();
-			pw.println(json);
-			pw.flush();		
-			
-		} else if (storename != null) {
-//			String type = req.getParameter("type");
-			String type = "diesel";
-			List<GasStation> gaslist = gasService.readGasByStorename(storename);
-			List<GasStation> priceGaslist = gasService.readGasPrice(gaslist.get(0).getRegion(), type);
-			String dieselprice = gaslist.get(0).getDiesel();
-			String gasolineprice = gaslist.get(0).getGasoline();
-			
-			for (int i = 0; i < priceGaslist.size(); i++) {
-				priceGaslist.get(index)
-				gaslist.get(0).getStorename()
-			}
-			
-			String pricejson = "{\"dieselprice\": " + dieselprice + 
-								", \"gasolineprice\": " + gasolineprice + 
-								", \"pricenum\": " + pricenum + "}";
-			PrintWriter pw = resp.getWriter();
-			pw.println(pricejson);
-			pw.flush();	
-		}	
+   @Override
+   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      String storename = req.getParameter("name");
+      System.out.println("name" + storename);
+      
+      if(storename == null) {
+         List<String> list = gasService.readGasXY();
+         // x,y 표값 받아온다
+         // 맵핑해서 보낸다
+         
+         ObjectMapper mapper = new ObjectMapper();
+         String json = mapper.writeValueAsString(list);
+         
+         PrintWriter pw = resp.getWriter();
+         pw.println(json);
+         pw.flush();      
+         
+      } else if (storename != null) {
+//         String type = req.getParameter("type");
+         String type = "diesel";
+         int pricenum = 0;
+         List<GasStation> gaslist = gasService.readGasByStorename(storename);
+         List<GasStation> priceGaslist = gasService.readGasPrice(gaslist.get(0).getRegion(), type);
+         String dieselprice = gaslist.get(0).getDiesel();
+         String gasolineprice = gaslist.get(0).getGasoline();
+         
+         for (int i = 0; i < priceGaslist.size(); i++) {
+            if(priceGaslist.get(i).getStorename().equals(gaslist.get(0).getStorename())) {
+               pricenum = (i + 1);
+            }
+         }
+         
+         String pricejson = "{\"dieselprice\": " + dieselprice + 
+                        ", \"gasolineprice\": " + gasolineprice + 
+                        ", \"pricenum\": " + pricenum + "}";
+         PrintWriter pw = resp.getWriter();
+         pw.println(pricejson);
+         pw.flush();   
+      }   
 
-	}
+   }
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String region = req.getParameter("region");
-		String storename = req.getParameter("storename");
-		System.out.println("region" + region);
-		System.out.println("storename" + storename);
+   @Override
+   protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      String region = req.getParameter("region");
+      String storename = req.getParameter("storename");
+      System.out.println("region" + region);
+      System.out.println("storename" + storename);
 
-		if (region != null) {
-			List<GasStation> list = gasService.readGasByRegion(region);
-			ObjectMapper mapper = new ObjectMapper();
-			String json = mapper.writeValueAsString(list);
+      if (region != null) {
+         List<GasStation> list = gasService.readGasByRegion(region);
+         ObjectMapper mapper = new ObjectMapper();
+         String json = mapper.writeValueAsString(list);
 
-			PrintWriter pw = resp.getWriter();
-			pw.println(json);
-			pw.flush();
+         PrintWriter pw = resp.getWriter();
+         pw.println(json);
+         pw.flush();
 
-		} else if (storename != null) {
-			List<GasStation> list = gasService.readGasByStorename(storename);
-			ObjectMapper mapper = new ObjectMapper();
-			String json = mapper.writeValueAsString(list);
+      } else if (storename != null) {
+         List<GasStation> list = gasService.readGasByStorename(storename);
+         ObjectMapper mapper = new ObjectMapper();
+         String json = mapper.writeValueAsString(list);
 
-			PrintWriter pw = resp.getWriter();
-			pw.println(json);
-			pw.flush();
-		}
-	}
+         PrintWriter pw = resp.getWriter();
+         pw.println(json);
+         pw.flush();
+      }
+   }
 
-	private GasStation jsonToGasStation(String json) throws JsonMappingException, JsonProcessingException {
-		ObjectMapper mapper = new ObjectMapper();
-		return mapper.readValue(json, GasStation.class);// 읽고 book class에 맵핑 후 반환
-	}
+   private GasStation jsonToGasStation(String json) throws JsonMappingException, JsonProcessingException {
+      ObjectMapper mapper = new ObjectMapper();
+      return mapper.readValue(json, GasStation.class);// 읽고 book class에 맵핑 후 반환
+   }
 
 }
